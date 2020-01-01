469cfe17cccbdc409858ce42413e671d278795f3
harshareddy832 <53609097+harshareddy832@users.noreply.github.com>
2020-01-01 14:27:24 +0530

    Add files via upload

---
+++ Captcha breaking/captcha.py
@@ -0,0 +1,20 @@
+from PIL import Image
+import pytesseract #gets text from image
+captcha = Image.open('do.jpg') #opens the image file
+breaker = pytesseract.image_to_string(captcha) #gets text from the selected jpg and stores the text
+result=0
+a=int(breaker[0]) #as they are in string format convert them integer by having the string inside this parathasis int()
+b=breaker[-2]
+c=int(breaker[-1])
+if b=="+":
+	result=a+c
+elif b=="-":
+	result=a-c
+elif b=="*":
+	result=a*c
+elif b=="/":
+	result=a/c
+else:
+	result="can't be done"
+print(result)  
+	
