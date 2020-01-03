b4a08e939c0b98b708f83eb314d0143d51c0fe19
harshareddy832 <53609097+harshareddy832@users.noreply.github.com>
2020-01-03 18:35:06 +0530

    Update rustmail.rs

--- rusted mail/rustmail.rs
+++ rusted mail/rustmail.rs
@@ -1,12 +1,12 @@
 use regex::Regex;
-
+//A regular expression is a special sequence of characters that helps you match or find other strings or sets of strings, using a specialized syntax held in a pattern.
 fn main() 
 {
-let re : Regex = Regex::new( re r"^([a-zA-z0-9_\-\.]+)@([a-zA-z0-9_\-\.])\.([a-zA-Z]{2,5})$").unwrap();
-if(re.is_match(text: "example@gmail.com")){}
-println!("valid email");
+let re : Regex = Regex::new( re r"^([a-zA-z0-9_\-\.]+)@([a-zA-z0-9_\-\.])\.([a-zA-Z]{2,5})$").unwrap();//these are random combinations of numbers,patterns and alphabets.
+if(re.is_match(text: "example@gmail.com")){}//checks whether valid or invalid
+println!("This is a valid email");
 }
 else{
-	println(" Not a valid account ")
+	println(" This is an invalid account ")
 }
 }
